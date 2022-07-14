bac571e8c20700cb1e2e1be4106ebb71f0cfdac5
SujungAhn <108320683+SujungAhn@users.noreply.github.com>
2022-07-14 17:50:31 +0900

    Add files via upload

---
+++ script/popup.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('.yy').click(function(){
+        $('.login').fadeIn();//.show();
+    });
+    $('.login a').click(function(){
+        $('.login').fadeOut();//.hide();
+    });
+});
